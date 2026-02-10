b0fd83434d0e566d4ff6eb4a82268af9a9c70fbe
ddann74 <daniel74n@gmail.com>
2026-02-11 01:15:02 +1100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/api/status")
+def get_status():
+    return jsonify({
+        "status": "Online",
+        "stabilization": "Waiting for data",
+        "message": "The Brain is connected!"
+    })
+
+if __name__ == "__main__":
+    app.run()
